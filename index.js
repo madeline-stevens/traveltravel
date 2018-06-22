f42e63aae65501be21b1727c84c16c0b3f15f327
Madeline Stevens <madelinerstevens@me.com>
2018-06-22 09:31:52 -0700

    Rename app.js to index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function myFunction () {
+  var checkBox = document.getElementById('myCheck');
+  if (checkBox.checked == true){
+    alert('Cool! You feel happy!');
+  }
+}
+
+function myFunction2 () {
+  var checkBox = document.getElementById('myCheck2');
+  if (checkBox.checked == true){
+    alert('Cool! You feel happy!');
+  }
+}
+
+function myFunction3 () {
+  var checkBox = document.getElementById('myCheck3');
+  if (checkBox.checked == true){
+    alert('Cool! You feel happy!');
+  }
+}
